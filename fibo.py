47126b961b2da3a333ae056ceaf7596b9ea35535
Johana-Huzevkova <johca03@gmail.com>
2024-04-24 09:26:05 +0200

    funkce pro n prvku Fibonacciho posloupnosti

--- fibo.py
+++ fibo.py
@@ -1,6 +1,18 @@
+def recursive_nth_fibo(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return recursive_nth_fibo(n - 1) + recursive_nth_fibo(n - 2)
+
 
 def main():
-    pass
+    n = int(input("Zadej počet členů Fibonacciho poslounosti:"))
+    seq = []
+    for num in range(n):
+        seq.append(recursive_nth_fibo(num))
+    print(seq)
 
 
 if __name__ == "__main__":
